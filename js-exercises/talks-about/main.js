9e7dec8d6c2a303e6d6626cedf420f9e13410fea
krulik <iwillkissyou@gmail.com>
2016-09-08 11:40:56 +0300

    mark matching element node

--- js-exercises/talks-about/main.js
+++ js-exercises/talks-about/main.js
@@ -4,7 +4,11 @@ console.log(talksAbout(document.body, "Politics")); // -> true
 
 function talksAbout (root, word) {
   if (root.nodeType === Node.TEXT_NODE) {
-    return root.nodeValue.toLowerCase().indexOf(word.toLowerCase()) !== -1;
+    var match = root.nodeValue.toLowerCase().indexOf(word.toLowerCase()) !== -1;
+    if (match) {
+      mark(root.parentNode);
+    }
+    return match;
   } else if (root.nodeType === Node.ELEMENT_NODE) {
     for (var i = 0; i < root.childNodes.length; i++) {
       if (talksAbout(root.childNodes[i], word)) {
@@ -13,4 +17,8 @@ function talksAbout (root, word) {
     }
     return false;
   }
+}
+
+function mark (node) {
+  node.style.color = 'red';
 }

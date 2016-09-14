e62befe237d849148f2ee9c8e4960e05da2770e8
krulik <iwillkissyou@gmail.com>
2016-09-14 20:12:21 +0300

    events ex.

---
+++ js-exercises/censored-keyboard/main.js
@@ -0,0 +1,22 @@
+var input = document.querySelector("input");
+
+input.addEventListener("keydown", censorKey);
+input.addEventListener("keydown", logKey);
+
+
+function censorKey (e) {
+  if (isBadKey(e.key)) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+}
+
+function isBadKey (key) {
+  var badKeys = ["q", "x", "w"];
+  return badKeys.indexOf(key) >= 0;
+}
+
+function logKey (e) {
+  var method = isBadKey(e.key) ? "error" : "log";
+  console[method]("User pressed '" + e.key + "'");
+}
